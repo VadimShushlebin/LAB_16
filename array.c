aae13ff3f8fe741ff202aa68f249a760bd71da68
PC <PC@DESKTOP-8MN65CJ>
2025-12-14 14:52:05 +0300

    Добавьте файлы проекта.

---
+++ array.c
@@ -0,0 +1,141 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#define _USE_MATH_DEFINES
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "array.h"
+
+
+double f(double x) {
+    return x * x * x - 4 * x * x + 2;
+}
+
+double* full_elements(double* ptr_array, int n) {
+    double x = 0.0;
+    for (int i = 0; i < n; i++) {
+        ptr_array[i] = f(x);
+        x += 0.5;
+    }
+    return ptr_array;
+}
+
+int find(int* arr, int size, int type) {
+    int fir = -1, last = -1;
+
+    for (int i = 0; i < size; i++) {
+        if ((type == 0 && arr[i] == 0) ||
+            (type == 1 && arr[i] > 0) ||
+            (type == 2 && arr[i] < 0)) {
+            fir = i;
+            break;
+        }
+    }
+
+    for (int i = size - 1; i >= 0; i--) {
+        if ((type == 0 && arr[i] == 0) ||
+            (type == 1 && arr[i] > 0) ||
+            (type == 2 && arr[i] < 0)) {
+            last = i;
+            break;
+        }
+    }
+
+    if (fir == -1 || last == -1 || fir >= last) {
+        return -1;
+    }
+
+    return fir + (last - fir) / 2;
+}
+
+double sum_elements(double* ptr_array, int begin, int end) {
+    double sum = 0.0;
+    for (int i = begin; i <= end; i++) {
+        sum += ptr_array[i];
+    }
+    return sum;
+}
+
+int find_element(double* ptr_array, int n, double element) {
+    for (int i = 0; i < n; i++) {
+        if (fabs(ptr_array[i] - element) < 0.000001) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+int find_min(double* ptr_array, int n, int k) {
+    int min_index = -1;
+    double min_v = 1000000.0;
+
+    for (int i = 0; i < n; i++) {
+        if (fabs(fmod(ptr_array[i], k)) < 0.000001) {
+            if (ptr_array[i] < min_v) {
+                min_v = ptr_array[i];
+                min_index = i;
+            }
+        }
+    }
+
+    return min_index;
+}
+
+
+double* full_elements_task2(double* ptr_array, int n) {
+    for (int i = 0; i < n; i++) {
+        ptr_array[i] = 0.1 * i - 0.5;
+    }
+    return ptr_array;
+}
+
+int put_elements(double* ptr_array, int n) {
+    for (int i = 0; i < n; i++) {
+        printf("%.3lf ", ptr_array[i]);
+    }
+    printf("\n");
+    return n;
+}
+
+double* calc_elements(double* ptr_array, int n) {
+    for (int i = 0; i < n; i++) {
+        ptr_array[i] = -ptr_array[i];
+    }
+    return ptr_array;
+}
+
+int delete_k(double* ptr_arr, int size, int k) {
+    if (k <= 0 || k > size) return size;
+
+    int size2 = 0;
+    for (int i = 0; i < size; i++) {
+        if ((i + 1) % k != 0) {
+            ptr_arr[size2] = ptr_arr[i];
+            size2++;
+        }
+    }
+    return size2;
+}
+
+double* insert_after_k(double* ptr_arr, int* size, int k) {
+    if (k <= 0 || k > *size) return ptr_arr;
+
+    int ins = (*size) / k;
+    int size_n = *size + ins;
+
+    double* new_arr = (double*)realloc(ptr_arr, size_n * sizeof(double));
+    if (new_arr == NULL) {
+        printf("Ошибка перевыделения памяти!\n");
+        return ptr_arr;
+    }
+
+    for (int i = *size - 1, j = size_n - 1; i >= 0; i--, j--) {
+        new_arr[j] = new_arr[i];
+        if ((i + 1) % k == 0) {
+            j--;
+            new_arr[j] = -999.0;
+        }
+    }
+
+    *size = size_n;
+    return new_arr;
+}
---
+++ main.c
@@ -0,0 +1,186 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include "array.h"
+#define N 10
+
+int main() {
+    system("chcp 1251");
+    puts("Лабораторные работы 11, 14, 16: Работа с массивами\n");
+
+
+    task11();
+
+    int size;
+    puts("Укажите размер массива");
+    scanf("%d", &size);
+
+    task14(size);
+    task16(size);
+
+    return 0;
+}
+
+void task11() {
+
+    float A[N], B[N];
+    float temp, summ = 0;
+
+    // Задание 1. Заполнение массива с консоли
+    for (int i = 0; i < N; i++) {
+        printf("a[%d] = ", i + 1);
+        scanf("%f", &temp);
+        A[i] = temp;
+        summ += A[i];
+    }
+
+    // Задание 2. Преобразование массива
+    for (int i = 0; i < N; i++) {
+        B[i] = -A[i];
+    }
+
+    printf("|Индекс|Исходное значение|Новое значение|\n");
+    for (int i = 0; i < N; i++) {
+        printf("|%-6d|%17.2f|%14.2f|\n", i, A[i], B[i]);
+    }
+
+    // ДЗ. Вычисление среднего значения элементов с четными индексами
+    int n;
+    float D[100];
+    printf("Введите размер списка: ");
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++) {
+        printf("a[%d] = ", i);
+        scanf("%f", &D[i]);
+    }
+
+    int co = 0;
+    float summ_dz = 0, sr_dz;
+    for (int i = 2; i < n; i += 2) {
+        summ_dz += D[i];
+        co++;
+    }
+
+    sr_dz = summ_dz / co;
+    printf("Среднее значение элементов, порядковые номера которых чётные: %.2f\n", sr_dz);
+}
+
+void task14(int size) {
+
+    double* array = (double*)malloc(size * sizeof(double));
+    if (array == NULL) {
+        printf("Ошибка выделения памяти\n");
+        return;
+    }
+
+    // Задание 1. Заполнение массива значениями из функции f(x)
+    full_elements(array, size);
+    put_elements(array, size);
+
+    // Задание 2. Преобразование массива
+    printf("\n\nПреобразованный массив\n");
+    calc_elements(array, size);
+    put_elements(array, size);
+
+    // Задание 3. Вычисление суммы элементов массива
+    int begin, end;
+    printf("\n\nВведите начальный индекс для суммирования: ");
+    scanf("%d", &begin);
+    printf("Введите конечный индекс для суммирования: ");
+    scanf("%d", &end);
+    printf("Сумма элементов %lf", sum_elements(array, begin, end));
+
+    // Задание 4. Поиск в массиве значения равного elem
+    double elem;
+    printf("\n\nВведите число для поиска: ");
+    scanf("%lf", &elem);
+    printf("Индекс искомого числа: %d", find_element(array, size, elem));
+
+    // Задание 5. Поиск порядкового номера минимального значения элементов, кратных k.
+    int k;
+    printf("\n\nВведите k для поиска минимального кратного элемента: ");
+    scanf("%d", &k);
+    int min_idx = find_min(array, size, k);
+    printf("Индекс минимального элемента, кратного %d: %d\n", k, min_idx);
+
+    free(array);
+
+    
+    int* arr = (int*)malloc(size * sizeof(int));
+
+    if (arr == NULL) {
+        printf("Ошибка выделения памяти\n");
+        return;
+    }
+
+    for (int i = 0; i < size; i++) {
+        printf("arr[%d]: ", i);
+        scanf("%d", &arr[i]);
+    }
+
+    printf("\nМассив: ");
+    for (int i = 0; i < size; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    // ДЗ. Поиск и вывод средних индексов
+    printf("Индекс между положительными: %d\n", find(arr, size, 1));
+    printf("Индекс между отрицательными: %d\n", find(arr, size, 2));
+    printf("Индекс между нулевыми: %d\n", find(arr, size, 0));
+
+    free(arr);
+}
+
+
+void task16(int size) {
+
+    double* ptr_array1 = (double*)malloc(size * sizeof(double));
+    if (ptr_array1 == NULL) {
+        puts("Ошибка выделения памяти");
+        return;
+    }
+
+    // Задание 1. Заполнение массива значениями функции f(x) 
+    full_elements(ptr_array1, size);
+    printf("Исходный массив:\n");
+    put_elements(ptr_array1, size);
+
+    // Задание 2. Преобразование массива
+    calc_elements(ptr_array1, size);
+    printf("Массив после преобразования:\n");
+    put_elements(ptr_array1, size);
+
+    free(ptr_array1);
+    int k;
+
+    double* ptr_array2 = (double*)malloc(size * sizeof(double));
+    if (ptr_array2 == NULL) {
+        puts("Ошибка выделения памяти");
+        return;
+    }
+
+    // 3адание 3. Заполнение массива
+    full_elements_task2(ptr_array2, size);
+    printf("Получившийся массив:\n");
+    put_elements(ptr_array2, size);
+
+    // Задание 4. Удаление каждого k-го элемента
+    printf("Введите k для удаления каждого k-го элемента: ");
+    scanf("%d", &k);
+    int size_after_delete = delete_k(ptr_array2, size, k);
+    printf("Массив после удаления каждого %d-го элемента:\n", k);
+    put_elements(ptr_array2, size_after_delete);
+    printf("Новый размер массива: %d\n", size_after_delete);
+
+    // Задание 5. Вставка -999 после каждого k-го элемента
+    printf("\nВведите k для вставки -999 после каждого k-го элемента: ");
+    scanf("%d", &k);
+    ptr_array2 = insert_after_k(ptr_array2, &size_after_delete, k);
+    printf("Массив после вставки -999 после каждого %d-го элемента:\n", k);
+    put_elements(ptr_array2, size_after_delete);
+    printf("Новый размер массива: %d\n", size_after_delete);
+
+    free(ptr_array2);
+}
